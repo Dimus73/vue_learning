459dcedae8f58b98ec572001e760e59ca2547ad0
Dimus73 <dmitry.prigozhin@gmail.com>
2023-08-29 18:12:16 +0300

    step 5

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './app.vue'
 
 import components from './Components/UI/index'
+import router from "@/router/router";
 
 const app = createApp(App)
 
@@ -9,4 +10,6 @@ components.forEach( component =>{
     app.component(component.name, component);
 })
 
-app.mount('#app')
+app
+    .use(router)
+    .mount('#app')
